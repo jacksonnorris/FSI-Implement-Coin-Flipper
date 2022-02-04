5474e3600a4c752681245c505e588823c02851b7
jacksonnorris <jacksonnorr@gmail.com>
2022-02-03 21:15:49 -0500

    Init commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,27 +1,65 @@
 // TODO: Declare any global variables we need
+let totalNum = 0
+let totalHeads = 0
+let totalTails = 0
+
+let resultImg = document.querySelector('#penny-result-img')
+let heads = document.querySelector('#heads')
+let headsPercent = document.querySelector('#heads-percent')
+let tails = document.querySelector('#tails')
+let tailsPercent = document.querySelector('#tails-percent')
+let resultContainer = document.querySelector('#result-container')
 
 
 document.addEventListener('DOMContentLoaded', function () {
-    // This is just a sanity check to make sure your JavaScript script is getting loaded
-    // You can remove it once you see it in your browser console in the developer tools
-    console.log('Hi')
-
-    // TODO: Add event listener and handler for flip and clear buttons
-
-    // Flip Button Click Handler
-        // TODO: Determine flip outcome
-        // TODO: Update image and status message in the DOM
-
-        // Update the scorboard
-        // TODO: Calculate the total number of rolls/flips
-        // Make variables to track the percentages of heads and tails
-        // TODO: Use the calculated total to calculate the percentages
-        // HINT: Make sure not to divide by 0! (if total is 0, percent will be 0 as well)
-        // TODO: Update the display of each table cell
-
-
-    // Clear Button Click Handler
-        // TODO: Reset global variables to 0
-        // TODO: Update the scoreboard (same logic as in flip button click handler)
-
+    document.querySelector('#flip-penny-button').addEventListener('click', function() {
+        let result = getRandomNum(2)
+        // console.log(result)
+        if (result == 1) { flipHeads() }
+        else if (result == 2) { flipTails() }
+        totalNum++
+        updateTable()
+    })
+    document.querySelector('#clear-results-button').addEventListener('click',function() {
+        totalNum = 0
+        totalHeads = 0
+        totalTails = 0
+        updateTable()
+    })
+    document.querySelector('#flip-n-times').addEventListener('click', function() {
+        let num = document.querySelector('#flip-num-times').value 
+        let flip = document.querySelector('#flip-penny-button')
+        for (let i = 0; i < num; i++) {
+            flip.click()
+        }
+    })
+        function getRandomNum(n) {
+            return Math.ceil(Math.random() * n)
+        }
+        function flipHeads() {
+            resultImg.src = 'assets/images/penny-heads.jpg'
+            // resultImg.setAttribute('class', 'penny-result-img')
+            resultImg.setAttribute('class', 'penny-result-img flipHead')
+            totalHeads++
+            resultContainer.innerHTML = "You Flipped Heads"
+        }
+        function flipTails() {
+            resultImg.src = 'assets/images/penny-tails.jpg'
+            // resultImg.setAttribute('class','penny-result-img')
+            resultImg.setAttribute('class','penny-result-img flipTail')
+            totalTails++
+            resultContainer.innerHTML = "You Flipped Tails"
+        }
+        function updateTable() {
+            heads.innerHTML = totalHeads
+            tails.innerHTML = totalTails
+            if (totalNum != 0) {
+                headsPercent.innerHTML = Math.round(100*totalHeads/totalNum) + "%"
+                tailsPercent.innerHTML = Math.round(100*totalTails/totalNum) + "%"
+            }
+            else {
+                headsPercent.innerHTML = "0%"
+                tailsPercent.innerHTML = "0%"
+            }
+        }
 })
